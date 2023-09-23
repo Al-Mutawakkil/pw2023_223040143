2296703b694d3d5d7ee043d74eb07e7bf01bbb94
Al-Mutawakkil <Thufails505@gmail.com>
2023-09-23 14:47:19 +0700

    include tugas 1

---
+++ tugas1/tugas1a.php
@@ -0,0 +1,10 @@
+<?php
+$topi = "Topi";
+$bundar = "Bundar";
+
+echo "<h2>";
+echo "<i>";
+echo "\"".$topi." saya ".$bundar.", ".$bundar." ".$topi." saya."."\"";
+echo "</i>";
+echo "</h2>";
+?>
---
+++ tugas1/tugas1b.php
@@ -0,0 +1,27 @@
+<?php
+$number = 43;
+$multiply = $number * 5;
+$division = $number / 2;
+$plus = $number + 75;
+$minus = $number - 20;
+
+echo "<p>";
+echo "Aku adalah angka <b>$number</b>";
+echo "</p>";
+
+echo "<p>";
+echo "Jika aku dikali 5, maka aku sekarang menjadi <b>$multiply</b>";
+echo "</p>";
+
+echo "<p>";
+echo "Jika aku dibagi 2, maka aku sekarang menjadi <b>$division</b>";
+echo "</p>";
+
+echo "<p>";
+echo "Jika aku ditambah 75, maka aku sekarang menjadi <b>$plus</b>";
+echo "</p>";
+
+echo "<p>";
+echo "Jika aku dikurang 20, maka aku sekarang menjadi <b>$minus</b>";
+echo "</p>";
+?>
